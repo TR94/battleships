7442f5d303be648555c834f2b26e1807a660dcfd
Tyrone Rutherford <tyrutherford@hotmail.co.uk>
2023-07-05 09:02:31 +0000

    add build computer board function

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ print ("Welcome to Battleships online game")
 # funcion needed to validate inputs
 
 name = "Ty"
-size = 9
+size = 3
 num_ships = 3
 
 class Board:
@@ -21,14 +21,23 @@ class Board:
         self.size = size
         self.num_ships = num_ships
 
-    #def player_board(self):
-        # return f"Your name: {self.name}, board size: {self.size}, number of battleships: {self.num_ships}"
+    # setting up blank board based on size input from player
+    def build_board(self):
+        blank_board = []
+        for i in range(self.size):
+            blank_board.append([])
+            for x in range(1, (self.size+1)):
+                blank_board[i].append(" * ")
+        return blank_board
 
-    def computer_board():
+    # Print computer board
+    def build_computer_board():
+        computer_board = build_board()
         for i in blank_board:
             for j in i:
                 print(j, end = " ")
             print() 
+
 
 def main ():
     player = Board(name, size, num_ships)
@@ -39,12 +48,14 @@ def main ():
 # main()
 
 # setting up blank board 
+"""
 blank_board = []
 for i in range(size):
     blank_board.append([])
     for x in range(1, (size+1)):
         blank_board[i].append(" * ")
-print(blank_board)
+return(blank_board)
+"""
 
 """
 print("Computer board")
@@ -56,3 +67,8 @@ print()
 def random_int(size):
     return randint(0, size-1)
 
+# action to print board from class 
+player = Board(name, size, num_ships)
+player.build_board()
+computer = Board("Computer", size, num_ships)
+computer.build_computer_board
