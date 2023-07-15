bad617fd009b64a8c03a261725ef569b6ad4b88b
Tyrone Rutherford <tyrutherford@hotmail.co.uk>
2023-07-15 12:48:54 +0000

    Update format with results from PEP8 checker

--- run.py
+++ run.py
@@ -7,9 +7,9 @@ def validate_size(data):
     try:
         int(data)
         if int(data) > 9:
-            raise ValueError(f"Board size cannot be larger than 9, you wrote {data}")
+            raise ValueError(f"Maximum board size 9, you wrote {data}")
         if int(data) < 4:
-            raise ValueError(f"Board size cannot be smaller than 4, you wrote {data}")
+            raise ValueError(f"Minimum board size 4, you wrote {data}")
 
     except ValueError as e:
         print(f"Invalid data: {e}, please try again. \n")
@@ -22,9 +22,11 @@ def validate_ships(size, num_ships):
     # Validates the number of ships data input
     try:
         if int(num_ships) > (int(size)-3)*4:
-            raise ValueError(f"Too many ships, you wrote {num_ships}. Please choose a smaller number")
+            raise ValueError(f"Too many ships, you wrote {num_ships}")
+            print("Please choose a smaller number")
         if int(num_ships) < 1:
-            raise ValueError(f"Too few ships, you wrote {num_ships}. Please choose at least 1 ship")
+            raise ValueError(f"Too few ships, you wrote {num_ships}")
+            print("Please choose at least 1 ship")
     except ValueError as e:
         print(f"Invalid data: {e}, please try again. \n")
         return False
@@ -39,7 +41,7 @@ def validate_guess(guess, size):
     try:
         int(guess)
         if int(guess) > (int(size)-1):
-            raise ValueError(f"Your guess was off the board, you wrote {guess}.")
+            raise ValueError(f"Your guess of '{guess}' was off the board")
             print(f"Please choose a number between 0 and ({guess_limit})")
     except ValueError as e:
         print(f"Invalid data: {e}, please try again. \n")
@@ -51,10 +53,8 @@ def validate_guess(guess, size):
 class Board:
     """
     Class to hold the details relevant to setting a game board.
-    This is used for the computer and game board.
-    Will hold the list of guesses made and ship positions.
-    Function to print the board for each round based on the information
-    held in the class.
+    Will hold the lists of grid, ship positions, guesses made and score.
+    Methods to process inputs and store data relevant to this class.
     """
     def __init__(self, name, size, num_ships, is_computer=False):
         self.name = name
@@ -78,26 +78,26 @@ class Board:
 
     def generate_ships(self):
         """
-        Takes the number of ships inputted from the player and random generated ship co-ords
-        and appends them to the ships list in the Board class
+        Takes the number of ships inputted from the player and random generated
+        ship co-ords and appends them to the ships list in the Board class
         """
         while len(self.ships) < (int(self.num_ships)):
             row, col = self.make_ship()
             ship_made = (str(row)+", "+str(col))
 
-            if self.does_ship_exist(ship_made) == False:
+            if self.does_ship_exist(ship_made) is False:
                 self.ships.append(ship_made)
-            
-            if self.is_computer == False:
+
+            if self.is_computer is False:
                 self.grid[row][col] = " @ "
 
     def does_ship_exist(self, ship_made):
-        # checks if the ship made already exists in ship list
+        # Checks if the ship made already exists in ship list
         if ship_made in self.ships:
             return True
         else:
             return False
-            
+
     def build_board(self):
         # Builds board based on class variables
         for row in range(0, int(self.size)):
@@ -151,7 +151,7 @@ class Board:
             print(f"{player_guess} has already been guessed, try again \n")
             guess_row, guess_col = self.player_guess()
             player_guess = guess_row + ", " + guess_col
-            
+
         else:
             self.guesses.append(player_guess)
 
@@ -159,18 +159,18 @@ class Board:
         hit = 0
         print("Player result: ")
 
-        for ship in computer.ships:
+        for ship in comp.ships:
             if ship == player_guess:
                 hit = 1
 
         if hit == 1:
             print("*** HIT ***\n")
-            computer.grid[int(guess_row)][int(guess_col)] = " X "
+            comp.grid[int(guess_row)][int(guess_col)] = " X "
             self.score += 1
 
         else:
             print("*** MISS ***\n")
-            computer.grid[int(guess_row)][int(guess_col)] = " O "
+            comp.grid[int(guess_row)][int(guess_col)] = " O "
 
     def computer_guess(self):
         # Generate random computer guess for each round
@@ -187,7 +187,7 @@ class Board:
         """
         guess_row, guess_col = self.computer_guess()
         computer_guess = str(guess_row) + ", " + str(guess_col)
-        
+
         # Checks the guess hasn't been made already
         while computer_guess in self.guesses:
             guess_row, guess_col = self.computer_guess()
@@ -238,20 +238,21 @@ def main():
     player.build_board()
     player.render_board()
 
-    global computer
-    computer = Board("Computer", size, num_ships, is_computer=True)
-    computer.build_board()
-    computer.render_board()
+    global comp
+    comp = Board("Computer", size, num_ships, is_computer=True)
+    comp.build_board()
+    comp.render_board()
 
     # Runs first round
     player.check_player_shot()
-    computer.check_computer_shot()
+    comp.check_computer_shot()
     print("Scores after this round:")
-    print(f"Player sunk {player.score} of {player.num_ships} battleships\n")
-    print(f"Computer sunk {computer.score} of {computer.num_ships} battleships\n")
+    print(f"Player sunk {player.score} of {player.num_ships} ships\n")
+    print(f"Computer sunk {comp.score} of {comp.num_ships} ships\n")
 
     # Runs sequential rounds until all ships are sunk
-    while computer.score < int(player.num_ships) and player.score < int(player.num_ships):
+    while comp.score < int(player.num_ships) and \
+            player.score < int(player.num_ships):
         play = input("Press any key to continue or 'q' to quit\n")
         if play == "q":
             print("Game ended")
@@ -261,14 +262,15 @@ def main():
             print("-"*20)
             print()
             player.render_board()
-            computer.render_board()
+            comp.render_board()
 
             player.check_player_shot()
-            computer.check_computer_shot()
-            print(f"Player sunk {player.score} of {player.num_ships} battleships\n")
-            print(f"Computer sunk {computer.score} of {computer.num_ships} battleships\n")
+            comp.check_computer_shot()
+            print("Scores this round: ")
+            print(f"Player sunk {player.score} of {player.num_ships} ships\n")
+            print(f"Computer sunk {comp.score} of {comp.num_ships} ships\n")
     else:
-        if computer.score >= int(player.num_ships):
+        if comp.score >= int(player.num_ships):
             print("***  Computer wins!!!  ***\n")
         else:
             print(f"***  {player.name} wins!!  ***\n")
